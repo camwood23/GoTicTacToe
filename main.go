1c5b40ff2eb75e4cc06d6b74ef79565a2c070d36
camwood23 <camwood23@gmail.com>
2015-06-03 18:25:00 -0400

    Board is now a 2D array.

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 //Need to add a win condition
+//Change everything to work with a 2D array
 package main
 
 import (
@@ -6,42 +7,63 @@ import (
     "strconv"
 )
 
-var board [9]string
+var board [3][3]string
 
 func main() {
-    i := 0
-    for i < 9 {
-        board[i] = strconv.Itoa(i)
+    i, j, a, player := 0, 0, 0, 0
+    for i < 3 {
+        for j < 3 {
+            board[i][j] = strconv.Itoa(a)
+            j += 1
+            a += 1
+        }
+        j = 0
         i += 1
     }
 
-    player := 0
-    //Game loop, need a win condition to break loop
-    for 1 > 0 {
+    for winCondition() {
+        i, j = 0, 0
         printBoard()
         fmt.Println("\nPlayer " + strconv.Itoa(player + 1) + " pick a space")
-        fmt.Scan(&i)
+        fmt.Scan(&a)
         fmt.Println()
-        if board[i] != "X" && board[i] != "O" {
-            if player == 0 {
-                board[i] = "X"
-            } else {
-                board[i] = "O"
+        for i < 3 {
+            for j < 3 {
+                if board[i][j] == strconv.Itoa(a) {
+                    if player == 0 {
+                        board[i][j] = "X"
+                    } else {
+                        board[i][j] = "O"
+                    }
+                    player = (player + 1) % 2
+                }
+                j += 1
             }
-            player = (player + 1) % 2
+            j = 0
+            i += 1
         }
     }
 }
 
+func winCondition() bool {
+    i, j := 0, 0
+    for i < 3 {
+        for j < 3 {
+            j += 1
+        }
+        j = 0
+        i += 1
+    }
+    return true
+}
+
 func printBoard() {
-    i := 0
-    j := 0
-    q := 0
+    i, j, a, q := 0, 0, 0, 0
     for i < 5 {
         if i % 2 == 0 {
             for j < 5 {
                 if j % 2 == 0 {
-                    fmt.Print(board[q]);
+                    fmt.Print(board[a][q]);
                     q += 1
                 } else {
                     fmt.Print("|");
@@ -52,6 +74,8 @@ func printBoard() {
             fmt.Println();
         } else {
             fmt.Println("-----");
+            q = 0
+            a += 1
         }
         i += 1
     }
